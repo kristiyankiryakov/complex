d46e4d51e7b89ef85f0bfc0d4e57691298e28b16
Kris <emineo@abv.bg>
2025-02-28 19:28:49 +0200

    re-deploy with aws policy changes

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const OtherPage = () => {
   return (
     <div>
-      Im some other page! Trigger re-deploy!
+      Im some other page! Trigger re-deploy! Again
       <Link to="/">Go back home</Link>
     </div>
   );
